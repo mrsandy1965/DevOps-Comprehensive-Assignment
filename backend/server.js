13ee1a2adbd0efab98fad66e0c24a8b54b9ac99d
mrsandy1965 <8055sandesh8055@gmail.com>
2026-04-26 11:08:06 +0530

    ready to ship

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,12 @@ connectDB();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-
+app.use(express.json())
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
 app.use("/api/users", userRoutes);
-
+app.use("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"../frontend/public","index.html"))
+})
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
